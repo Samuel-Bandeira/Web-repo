60755b4fa711723b13540a907e1bed80d3a3aeb4
Samuel Bandeira <samuel.bandeira.oliveira@gmail.com>
2020-08-29 13:57:00 -0300

    javascript o começo

---
+++ rps-game/script.js
@@ -0,0 +1,40 @@
+const playerScoreEl = document.getElementById('playerScore')
+const playerChoiceEl = document.getElementById('playerChoice')
+const computerScoreEl = document.getElementById('computerScore')
+const computerChoiceEl = document.getElementById('computerChoice')
+
+const playerRock = document.getElementById('playerRock')
+const playerPaper = document.getElementById('playerPaper')
+const playerScissors = document.getElementById('playerScissors')
+const computerLizard = document.getElementById('playerLizard')
+const playerSpock = document.getElementById('playerSpock')
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
+
+const allGameIcons = document.querySelectorAll('.far')
+const resultText = document.getElementById('resultText')
+
+const choices = {
+    rock: {name:'Rock', defeats: ['scissors', 'lizard']},
+    paper:{name:'Paper', defeats: ['rock', 'spock']},
+    scissors:{name:'Scissors', defeats: ['paper', 'lizard']},
+    lizard: {name:'Lizard', defeats: ['paper', 'spock']},
+    spock: {name:'Spock', defats: ['scissors', 'rock']}
+}
+
+let playerScoreNumber = 0
+let computerScoreNumber = 0
+let computerChoice = ''
+
+//Reset all selected itens.
+function resetSelected(){
+    allGameIcons.forEach((icon) => {
+        icon.classList.remove('selected')
+    });
+}
+
+//Reset socre & playerChoice/computerChoice
