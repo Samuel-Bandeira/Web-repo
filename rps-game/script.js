384835f712abced9ef73f559c2be4675e1437c1e
Samuel Bandeira <samuel.bandeira.oliveira@gmail.com>
2020-08-29 16:31:26 -0300

    random choices and reset

--- rps-game/script.js
+++ rps-game/script.js
@@ -38,3 +38,59 @@ function resetSelected(){
 }
 
 //Reset socre & playerChoice/computerChoice
+function resetAll() {
+    playerScoreNumber = 0
+    computerScoreNumber = 0
+    playerScoreEl.textContent = playerScoreNumber
+    computerScoreEl.textContent = computerScoreNumber
+    playerChoiceEl.textContent = ''
+    computerChoiceEl.textContent = ''
+    resultText.textContent = ''
+    resetSelected()
+}
+window.resetAll = resetAll
+
+function computerRandomChoice() {
+    const computerChoiceNumber = Math.random()
+   
+    if (computerChoiceNumber < 0.2) {
+        computerChoice = 'rock';
+      } else if (computerChoiceNumber <= 0.4) {
+        computerChoice = 'paper';
+      } else if (computerChoiceNumber <= 0.6) {
+        computerChoice = 'scissors';
+      } else if (computerChoiceNumber <= 0.8) {
+        computerChoice = 'lizard';
+      } else {
+        computerChoice = 'spock';
+      }
+}
+
+// Add 'selected' styling & computerChoice
+function displayComputerChoice() {
+    switch(computerChoice) {
+        case 'rock':
+            computerRock.classList.add('selected')
+            computerChoiceEl.textContent = ' --- Rock'
+            break
+        case 'paper':
+            computerPaper.classList.add('selected')
+            computerPaper.textContent = ' --- Paper'
+        case 'scissors':
+            computerScissors.classList.add('selected');
+            computerChoiceEl.textContent = ' --- Scissors';
+            break;
+        case 'lizard':
+            computerLizard.classList.add('selected');
+            computerChoiceEl.textContent = ' --- Lizard';
+            break;
+        case 'spock':
+            computerSpock.classList.add('selected');
+            computerChoiceEl.textContent = ' --- Spock';
+            break;
+        default:
+            break;
+    }
+}
+
+// Check result, increase scores, update resultText
